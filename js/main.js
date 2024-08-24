27e0144f440c4bcd5c1399e16105363c0932fb73
Phonyc <119613433+Phonyc@users.noreply.github.com>
2024-08-24 19:41:52 +0200

    Fixs + actualites

--- js/main.js
+++ js/main.js
@@ -1,14 +1,26 @@
+
+
 let mobileClosed = true;
 
+
+
+function setBtOp() {
+    document.getElementById('burgerButton').style.display = "none";
+    document.getElementById('closeButton').style.display = "block";
+}
+function setBtCl() {
+    document.getElementById('burgerButton').style.display = "block";
+    document.getElementById('closeButton').style.display = "none";
+}
 function show_ban() {
     if (mobileClosed) {
-
         if (window.matchMedia("(max-width: 992px)").matches) {
             document.querySelectorAll('.fermerMobile').forEach(function (el) {
                 el.style.display = "flex";
             });
             document.querySelector('body').style.overflow = "hidden";
             mobileClosed = false;
+            setBtOp();
         }
     } else {
         if (window.matchMedia("(max-width: 992px)").matches) {
@@ -17,6 +29,7 @@ function show_ban() {
             });
             document.querySelector('body').style.overflow = "auto";
             mobileClosed = true;
+            setBtCl()
         }
     }
 }
@@ -27,12 +40,14 @@ window.onresize = function(event) {
         });
         document.querySelector('body').style.overflow = "auto";
         mobileClosed = true;
+        setBtCl()
     } else {
         if (window.matchMedia("(max-width: 992px)").matches) {
             document.querySelectorAll('.fermerMobile').forEach(function (el) {
                 el.style.display = "none";
             });
             document.querySelector('body').style.overflow = "auto";
+            setBtCl()
             mobileClosed = true;
         }
 

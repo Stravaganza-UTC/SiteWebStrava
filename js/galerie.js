de026a5febd77e159723e6b08f852c9f988becaf
Phonyc <119613433+Phonyc@users.noreply.github.com>
2024-09-02 16:30:06 +0200

    Galerie - Affichage en grand

---
+++ js/galerie.js
@@ -0,0 +1,51 @@
+// Légende dans le alt
+// Si déjà un paragraphe, le alt ne devient pas légende
+let oldGalerieScroll = 0;
+const divs = Array.from(document.querySelectorAll('.grilleGalerie > div'));
+
+divs.forEach((div) => {
+    let alt = div.querySelector("img").alt;
+    // Vérifier qu'il n'y a pas de p dans les enfants
+    if (div.querySelector("p") == null) {
+        // Ajouter un p avec la légende
+        let p = document.createElement("p");
+        p.innerText = alt;
+        div.appendChild(p);
+    }
+    div.addEventListener('click', () => {
+        const index = divs.indexOf(div);
+        carousel(div, index);
+    });
+});
+
+function carousel(img, index) {
+    let grille = img.parentNode
+    if (grille.className !== "carouselGalerie") {
+        grille.className = "carouselGalerie"
+        grille.querySelectorAll('.btVisuImg').forEach(function (el) {
+            el.classList.remove("hidden");
+        });
+        grille.scrollTo(window.innerWidth * index, 0);
+        oldGalerieScroll = window.scrollY;
+    }
+}
+
+function carouselClose(bt) {
+    window.scrollTo(0, oldGalerieScroll);
+    let grille = bt.parentNode;
+    grille.className = "grilleGalerie";
+    grille.querySelectorAll('.btVisuImg').forEach(function (el) {
+
+        el.classList.add("hidden");
+    });
+}
+
+function carouselScrollRight(bt) {
+    let carousel = bt.parentNode;
+    carousel.scrollTo(carousel.scrollLeft + window.innerWidth, 0);
+}
+
+function carouselScrollLeft(bt) {
+    let carousel = bt.parentNode;
+    carousel.scrollTo(carousel.scrollLeft - window.innerWidth, 0);
+}
--- js/main.js
+++ js/main.js
@@ -1,17 +1,16 @@
-
-
 let mobileClosed = true;
-
 
 
 function setBtOp() {
     document.getElementById('burgerButton').style.display = "none";
     document.getElementById('closeButton').style.display = "block";
 }
+
 function setBtCl() {
     document.getElementById('burgerButton').style.display = "block";
     document.getElementById('closeButton').style.display = "none";
 }
+
 function show_ban() {
     if (mobileClosed) {
         if (window.matchMedia("(max-width: 992px)").matches) {
@@ -33,7 +32,8 @@ function show_ban() {
         }
     }
 }
-window.onresize = function(event) {
+
+window.onresize = function (event) {
     if (window.innerWidth > 992) {
         document.querySelectorAll('.fermerMobile').forEach(function (el) {
             el.style.display = "flex";
@@ -54,3 +54,4 @@ window.onresize = function(event) {
     }
 
 };
+
